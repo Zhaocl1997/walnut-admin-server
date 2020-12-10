b1a48ab8d5c2cdcc93b05d0fdc51a617a11167af
zhaocl1997 <674351624@qq.com>
2020-12-10 21:54:26 +0800

    feat: add logger middleware

--- src/walnut/app/middleware/logger.ts
+++ src/walnut/app/middleware/logger.ts
@@ -1,10 +1,23 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: Function) {
-    console.log(req.body);
+  private logger = new Logger('HTTP');
+
+  use(request: Request, response: Response, next: NextFunction): void {
+    const { ip, method, baseUrl } = request;
+    const userAgent = request.get('user-agent') || '';
+
+    response.on('finish', () => {
+      const { statusCode } = response;
+      const contentLength = response.get('content-length');
+
+      this.logger.log(
+        `${method} ${baseUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip}`,
+      );
+    });
+
     next();
   }
 }
