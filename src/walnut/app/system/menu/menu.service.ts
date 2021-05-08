fa93955daf51d6c033bdefd19d4c8e5f400a1473
zhaocl1997 <674351624@qq.com>
2021-05-08 19:10:45 +0800

    fix: menu response data

--- src/walnut/app/system/menu/menu.service.ts
+++ src/walnut/app/system/menu/menu.service.ts
@@ -9,16 +9,16 @@ import { ResponseSuccess } from '../../shared/response';
 export class MenuService {
   constructor(@InjectModel(Menu.name) private menuModel: Model<Menu>) {}
 
-  async findById(id: Schema.Types.ObjectId): Promise<Menu> {
-    return await this.menuModel.findById(id);
+  async findById(id: Schema.Types.ObjectId) {
+    return ResponseSuccess(await this.menuModel.findById(id));
   }
 
-  async create(dto: Menu): Promise<Menu> {
-    return await this.menuModel.create(dto);
+  async create(dto: Menu) {
+    return ResponseSuccess(await this.menuModel.create(dto));
   }
 
-  async read(id: Schema.Types.ObjectId): Promise<Menu> {
-    return await this.menuModel.findOne({ _id: id, deleted: false });
+  async read(id: Schema.Types.ObjectId) {
+    return ResponseSuccess(await this.menuModel.findOne({ _id: id, deleted: false }));
   }
 
   async update(dto: Menu) {
@@ -35,10 +35,10 @@ export class MenuService {
       throw new Error('Menu does not found');
     }
 
-    return oldMenu;
+    return ResponseSuccess(oldMenu);
   }
 
-  async delete(id: Schema.Types.ObjectId): Promise<Menu> {
+  async delete(id: Schema.Types.ObjectId) {
     const deletedMenu = await this.menuModel.findOne({
       _id: id,
       deleted: false,
@@ -49,8 +49,10 @@ export class MenuService {
     }
 
     deletedMenu.deleted = true;
+
     await deletedMenu.save();
-    return deletedMenu;
+
+    return ResponseSuccess(deletedMenu);
   }
 
   async findAll(): Promise<any> {
@@ -70,9 +72,9 @@ export class MenuService {
     const icon = await this.menuModel.find({ deleted: false });
     const ret = [...new Set(icon.map((item) => item.icon).filter((i) => i))];
 
-    return {
+    return ResponseSuccess({
       data: ret,
       total: ret.length,
-    };
+    }) 
   }
 }
