87190b2c2e308d74b995cdd08161625f6d159141
Zhaocl1997 <zhaocl97@foxmail.com>
2022-12-21 16:12:54 +0800

    refactor: remove useless i18n config service

--- src/walnut/app/i18n/i18n.module.ts
+++ src/walnut/app/i18n/i18n.module.ts
@@ -10,14 +10,10 @@ import {
 import { join } from 'path';
 
 import { AppConstHeaders } from '@/const/app/header';
-import { I18nConfigService } from './i18n.config.service';
 
 @Module({
   imports: [
     I18nModule.forRootAsync({
-      // TODO i18n do not support use class yet
-      // useClass: I18nConfigService,
-
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         fallbackLanguage: configService.get('app.i18n.fallback'),
