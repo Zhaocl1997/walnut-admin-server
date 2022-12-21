c944d2598bec56224963418af01416638a47c552
Zhaocl1997 <zhaocl97@foxmail.com>
2022-12-21 16:13:21 +0800

    refactor: repo version

--- src/walnut/app/middleware/src/version.middleware.ts
+++ src/walnut/app/middleware/src/version.middleware.ts
@@ -2,8 +2,7 @@ import { AppConstHeaders } from '@/const/app/header';
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NextFunction } from 'express';
-
-// https://stackoverflow.com/questions/55753163/package-json-is-not-under-rootdir/61467483#61467483
+const packageJson = require('../../../../../package.json');
 
 @Injectable()
 export class VersionMiddleware implements NestMiddleware {
@@ -15,9 +14,8 @@ export class VersionMiddleware implements NestMiddleware {
     req.headers[AppConstHeaders.VERSION] = version;
     req.version = version;
 
-    // TODO json resolve in tsconfig will cause dist structure error
-    req.headers[AppConstHeaders.REPO_VERSION] = '1.0.0';
-    req.repoVersion = '1.0.0';
+    req.headers[AppConstHeaders.REPO_VERSION] = packageJson.version;
+    req.repoVersion = packageJson.version;
 
     next();
   }
